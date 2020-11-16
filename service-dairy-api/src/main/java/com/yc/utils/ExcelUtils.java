385b12bdab28d333a0fea3a5eaff58012ed43b41
yc100 <1301760713@qq.com>
2020-11-16 16:50:11 +0800

    增加工具类

--- service-dairy-api/src/main/java/com/yc/utils/ExcelUtils.java
+++ service-dairy-api/src/main/java/com/yc/utils/ExcelUtils.java
@@ -71,4 +71,23 @@ public class ExcelUtils {
 
         exportExcel(response, fileName, targetList, targetClass);
     }
+
+    /**
+     * 颜色:十六进制转rgb
+     * @author YC
+     * @createDate 2020/11/13 16:52
+     **/
+    public static int [] hexToRgb(String hex){
+        String colorStr = hex;
+        if (hex.startsWith("#")) {
+            colorStr = hex.substring(1);
+        }
+        if (StringUtils.length(colorStr) == 8) {
+            colorStr = hex.substring(2);
+        }
+        int  r=  Integer.valueOf( colorStr.substring( 0, 2 ), 16 );
+        int  g=  Integer.valueOf( colorStr.substring( 2, 4 ), 16 );
+        int  b=  Integer.valueOf( colorStr.substring( 4, 6 ), 16 );
+        return new int[] { r, g, b };
+    }
 }
